e1d8d4f3e83fac9b539853e858c4a2836ae082d9
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-09-09 00:57:02 +0530

    Add Padding/Margin on Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,75 +24,21 @@ class DashBoardScreen extends StatelessWidget{
         title: Text('DashBoard'),
         backgroundColor: Colors.blue,
       ),
-      // body: Container(
-      //   color: Colors.blue.shade50,
-      //   width: double.infinity,
-      //   height: double.infinity,
-      //   child: Center(
-      //     child: Container(
-      //       width: 100,
-      //       height: 100,
-      //       // color: Colors.blueGrey,
-      //       decoration: BoxDecoration(
-      //         color: Colors.blueGrey,
-      //         // borderRadius: BorderRadius.circular(10),
-      //         // borderRadius: BorderRadius.only(
-      //         //   topLeft: Radius.circular(10),
-      //         //   topRight: Radius.circular(10),
-      //         // )
-      //         border: Border.all(
-      //           color: Colors.black,
-      //           width: 2,
-      //         ),
-      //         boxShadow: const [
-      //           BoxShadow(
-      //             color: Colors.black,
-      //             offset: Offset(5, 5),
-      //             blurRadius: 51,
-      //             spreadRadius: 5,
-      //           ),
-      //         ],
-      //         shape: BoxShape.circle,
-      //       ),
-      //     ),
-      //   ),
+      // body: const Padding(
+      //   // padding: const EdgeInsets.all(8.0),
+      //   padding: EdgeInsets.only(top: 11,left:111),
+      //   child: Text("Jai Shree Ram",style: TextStyle(fontSize: 25),),
       // )
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.blue,
-            ),
+      body: Container(
+        color: Colors.blueGrey,
+        margin: EdgeInsets.all(20),
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(11),
+            child: Text("Jai Shree Ram",style: TextStyle(fontSize: 25,color: Colors.white),),
           ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.orange,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.blueGrey,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.green,
-            ),
-          ),
-        ],
-      ),
+        ),
+      )
     );
   }
 
