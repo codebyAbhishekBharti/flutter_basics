b02ef22f33ca587350dd1f747ff3c7b750465197
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-09-09 00:48:22 +0530

    Expanded Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -24,39 +24,75 @@ class DashBoardScreen extends StatelessWidget{
         title: Text('DashBoard'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        color: Colors.blue.shade50,
-        width: double.infinity,
-        height: double.infinity,
-        child: Center(
-          child: Container(
-            width: 100,
-            height: 100,
-            // color: Colors.blueGrey,
-            decoration: BoxDecoration(
-              color: Colors.blueGrey,
-              // borderRadius: BorderRadius.circular(10),
-              // borderRadius: BorderRadius.only(
-              //   topLeft: Radius.circular(10),
-              //   topRight: Radius.circular(10),
-              // )
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
-              ),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black,
-                  offset: Offset(5, 5),
-                  blurRadius: 51,
-                  spreadRadius: 5,
-                ),
-              ],
-              shape: BoxShape.circle,
+      // body: Container(
+      //   color: Colors.blue.shade50,
+      //   width: double.infinity,
+      //   height: double.infinity,
+      //   child: Center(
+      //     child: Container(
+      //       width: 100,
+      //       height: 100,
+      //       // color: Colors.blueGrey,
+      //       decoration: BoxDecoration(
+      //         color: Colors.blueGrey,
+      //         // borderRadius: BorderRadius.circular(10),
+      //         // borderRadius: BorderRadius.only(
+      //         //   topLeft: Radius.circular(10),
+      //         //   topRight: Radius.circular(10),
+      //         // )
+      //         border: Border.all(
+      //           color: Colors.black,
+      //           width: 2,
+      //         ),
+      //         boxShadow: const [
+      //           BoxShadow(
+      //             color: Colors.black,
+      //             offset: Offset(5, 5),
+      //             blurRadius: 51,
+      //             spreadRadius: 5,
+      //           ),
+      //         ],
+      //         shape: BoxShape.circle,
+      //       ),
+      //     ),
+      //   ),
+      // )
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: 50,
+              height: 100,
+              color: Colors.blue,
             ),
           ),
-        ),
-      )
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: 50,
+              height: 100,
+              color: Colors.orange,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              width: 50,
+              height: 100,
+              color: Colors.blueGrey,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: 50,
+              height: 100,
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
