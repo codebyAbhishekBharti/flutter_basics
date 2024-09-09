431c2d71c67e9273457aad785e5533bf523a33f4
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-09-10 01:12:02 +0530

    Adding Time to App with start and stop button using statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,41 @@ import 'package:flutter_basics/ui_helper/util.dart';
 void main() {
   runApp(FlutterApp());
 }
-class FlutterApp extends StatelessWidget{
+
+class FlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Flutter App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: const TextTheme(
-            displayLarge: TextStyle(
-              fontFamily: 'FontMain',
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-            displayMedium: TextStyle(
-              fontFamily: 'FontMain',
-              fontSize: 11,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic,
-            ),
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontFamily: 'FontMain',
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+          displayMedium: TextStyle(
+            fontFamily: 'FontMain',
+            fontSize: 11,
+            fontWeight: FontWeight.w500,
+            fontStyle: FontStyle.italic,
           ),
         ),
-        home:   DashBoardScreen(),
-      );
+      ),
+      home: DashBoardScreen(),
+    );
   }
 }
 
-class DashBoardScreen extends StatelessWidget{
-  var emailText = TextEditingController();
-  var password = TextEditingController();
+class DashBoardScreen extends StatefulWidget {
+  @override
+  _DashBoardScreenState createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  var time = DateTime.now();
+  var flag_time=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,89 +48,57 @@ class DashBoardScreen extends StatelessWidget{
       ),
       body: Center(
         child: Container(
-          width: 300,
+          width: 200,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                // enabled: false,
-                  controller: emailText,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    // suffixText: 'Suffix',
-                    hintText: 'Enter Email',
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.search),
-                      onPressed: (){
-                        print('Search Icon Clicked');
-                      },
-                    ),
-                    prefixIcon: Icon(Icons.email, color: Colors.red,),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                        width: 5,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                        width: 2,
-                      ),
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 2,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                        width: 2,
-                      ),
-                    ),
-
-                  ),
-              ),
-              Container(height: 11,),
-              TextField(
-                controller: password,
-                obscureText: true,
-                obscuringCharacter: '*',
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Enter Password',
-                  prefixIcon: Icon(Icons.lock, color: Colors.red,),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 2,
-                    ),
-                  ),
-
+              Text(
+                '${time.hour}:${time.minute}:${time.second}',
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               ElevatedButton(
-                  onPressed: (){
-                    print('Email: ${emailText.text}');
-                    print('Password: ${password.text}');
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Login', style: TextStyle(fontSize: 20,color: Colors.red),),
-                  )
+                onPressed: () async {
+                  // for(int i=0; i<10; i++) {
+                  // //   print(i);
+                  // //   await Future.delayed(Duration(seconds: 1));
+                  //   print("sleep 1 sec");
+                  // }
+                  for(int i=0;i<100;i++){
+                    if(flag_time==false){
+                      break;
+                    }
+                    print(flag_time);
+                    print(i);
+                    setState(() {
+                      time = DateTime.now();
+                    });
+                    await Future.delayed(Duration(seconds: 1));
+                  };
+                  // setState(() {
+                  //   time = DateTime.now();
+                  // });
+                },
+                child: Text("Current Time"),
               ),
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      flag_time = false;
+                    });
+                    print("Stop time flag ${flag_time}");
+                    print("Stopping time");
+                  }
+                  ,
+                  child: Text('Stop Time')
+              )
             ],
           ),
         ),
-      )
-      );
+      ),
+    );
   }
-
 }
