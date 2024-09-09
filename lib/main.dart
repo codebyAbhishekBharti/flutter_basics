6a05d414d99e521a378edf6428f1780f25b07c8b
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-09-09 10:54:11 +0530

    Flutter Styles and Themes, Custom Font file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/ui_helper/util.dart';
 
 void main() {
   runApp(FlutterApp());
@@ -10,6 +11,19 @@ class FlutterApp extends StatelessWidget{
         title: 'Flutter App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              fontFamily: 'FontMain',
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+            displayMedium: TextStyle(
+              fontFamily: 'FontMain',
+              fontSize: 11,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
         ),
         home:   DashBoardScreen(),
       );
@@ -22,15 +36,20 @@ class DashBoardScreen extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text('DashBoard'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.orange,
       ),
-      body: Center(
-        child: Text("Jai Shree Ram",
-            style: TextStyle(
-              fontFamily: 'FontMain',
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),),
+      body:  Column(
+        children: [
+          Text("Jai Shree Ram",
+            style: Theme.of(context).textTheme.displayLarge!.copyWith(color: Colors.redAccent),),
+          Text("Jai Shree Ram",
+            style: Theme.of(context).textTheme.displayMedium,),
+          Text("Jai Shree Ram",
+            style: Theme.of(context).textTheme.displayLarge,),
+          Text("Jai Shree Ram",
+            style: mTextStyle11(),
+          ),
+        ]
       ),
     );
   }
---
+++ lib/ui_helper/util.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+TextStyle mTextStyle11() {
+  return const TextStyle(
+    fontFamily: 'FontMain',
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.italic,
+  );
+}
+
+TextStyle mTextStyle21() {
+  return const TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.italic,
+  );
+}
