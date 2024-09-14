3a1f9b45a4f6d81a763e2dedbb3ec21793f92022
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-09-15 00:07:26 +0530

    callBack function implementation

--- lib/main.dart
+++ lib/main.dart
@@ -37,28 +37,9 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
-  var arrColor = [
-    Colors.amber,
-    Colors.blue,
-    Colors.green,
-    Colors.red,
-    Colors.purple,
-    Colors.orange,
-    Colors.pink,
-    Colors.teal,
-    Colors.yellow,
-    Colors.brown,
-    Colors.cyan,
-    Colors.indigo,
-    Colors.lime,
-    Colors.grey,
-    Colors.deepOrange,
-    Colors.deepPurple,
-    Colors.lightBlue,
-    Colors.lightGreen,
-    Colors.blueGrey,
-    Colors.teal,
-  ];
+  callBack() {
+    print("Button Clicked");
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,28 +47,9 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
         title: Text('Jai Shree Ram'),
         backgroundColor: Colors.orange,
       ),
-      body: Center(
-        child: Container(
-          width: 250,
-          margin: EdgeInsets.all(10),
-          child: GridView.count(
-            crossAxisCount: 3,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            children: [
-              Container(color: arrColor[0]),
-              Container(color: arrColor[1]),
-              Container(color: arrColor[2]),
-              Container(color: arrColor[3]),
-              Container(color: arrColor[4]),
-              Container(color: arrColor[5]),
-              Container(color: arrColor[6]),
-              Container(color: arrColor[7]),
-              Container(color: arrColor[8]),
-            ],
-          ),
-        ),
-      )
+      body: ElevatedButton(
+          onPressed: callBack,
+          child: Text("Clicke Me"))
     );
   }
 }
